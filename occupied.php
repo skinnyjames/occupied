608cc641dfec239902e63e510a6e1675c05bbe55
Sean Gregory <seangregory@gmail.com>
2017-10-01 01:59:49 -0400

    add license

--- occupied.php
+++ occupied.php
@@ -4,6 +4,7 @@ Plugin Name: occupied
 Description: custom page locking for WordPress admin pages
 Version: 0.1.0
 Author: skinnyjames
+License: GPLv3
 */
 
 require_once('occupied/class.occupied.php');
