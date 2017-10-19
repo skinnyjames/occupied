ade627e85b35547196cc84d4ba79283bbcc3c115
Sean Gregory <seangregory@adams.net>
2017-10-19 10:37:02 -0500

    remove test menu page

--- occupied.php
+++ occupied.php
@@ -8,20 +8,6 @@ License: GPLv3
 */
 
 require_once('occupied/class.occupied.php');
-
 Occupied::register_hooks();
-
-/* Demo */
-add_action('admin_menu', 'occupied_register_test_menu');
-
-function occupied_register_test_menu(){
-  add_menu_page("Occupied Test","occupied-test","manage_options", "occupied_test_page", "occupied_test_menu", "dashicons-heart",7);
-}
-
-function occupied_test_menu(){
-  $lock = Occupied::protect();
-
-  echo "<h1>Hello World</h1>";
-}
 
 ?>
